bf49ff6d2edc8c4565633b15a18b07d6fe174590
Syaefulloh Arnas <92679271+syaeful16@users.noreply.github.com>
2025-05-14 09:02:02 +0700

    Update database.go

--- database/database.go
+++ database/database.go
@@ -17,7 +17,7 @@ func ConnectDB() {
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
-	dbPassword := os.Getenv("DB_PASSWORD")
+	dbPassword := os.Getenv("DB_PASS")
 	dbUser := os.Getenv("DB_USER")
 
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPassword, dbName)
